39b0e20166017f363b3f8d77a33462e35165ac67
Sachin Gurung <saching061@hotmail.com>
2021-03-10 23:48:35 -0800

    Using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 import { ScrollView, Text, FlatList } from "react-native";
-import { PARTNERS } from "../shared/partners";
-import {Card, ListItem } from "react-native-elements";
+import { Card, ListItem } from "react-native-elements";
+import { connect } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
+
+const mapStateToProps = (state) => {
+  return {
+    partners: state.partners,
+  };
+};
 
 const Mission = () => {
   return (
@@ -20,15 +27,8 @@ const Mission = () => {
 };
 
 class About extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      partners: PARTNERS,
-    };
-  }
-
   static navigationOptions = {
-    title: "About",
+    title: "About Us",
   };
 
   render() {
@@ -37,7 +37,7 @@ class About extends Component {
         <ListItem
           title={item.name}
           subtitle={item.description}
-          leftAvatar={{ source: require("./images/bootstrap-logo.png") }}
+          leftAvatar={{source: {uri: baseUrl + item.image}}}
         />
       );
     };
@@ -45,15 +45,15 @@ class About extends Component {
       <ScrollView>
         <Mission />
         <Card title="Community Partners">
-        <FlatList
-                data={this.state.partners}
-                renderItem={renderPartner}
-                keyExtractor={item => item.id.toString()}
-            />
+          <FlatList
+            data={this.props.partners.partners}
+            renderItem={renderPartner}
+            keyExtractor={(item) => item.id.toString()}
+          />
         </Card>
       </ScrollView>
     );
   }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,73 +1,84 @@
-import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList } from 'react-native';
-import { Card } from 'react-native-elements';
-import { CAMPSITES } from '../shared/campsites';
-import { COMMENTS } from '../shared/comments';
+import React, { Component } from "react";
+import { Text, View, ScrollView, FlatList } from "react-native";
+import { Card } from "react-native-elements";
+import { connect } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
 
-function RenderCampsite({campsite}) {
+const mapStateToProps = (state) => {
+  return {
+    campsites: state.campsites,
+    comments: state.comments,
+  };
+};
 
-    if (campsite) {
-        return (
-            <Card
-                featuredTitle={campsite.name}
-                image={require('./images/react-lake.jpg')}
-            >
-                <Text style={{margin: 10}}>
-                    {campsite.description}
-                </Text>
-            </Card>
-        );
-    }
-    return <View />;
+function RenderCampsite({ campsite }) {
+  if (campsite) {
+    return (
+      <Card
+        featuredTitle={campsite.name}
+        image={{ uri: baseUrl + campsite.image }}
+      >
+        <Text style={{ margin: 10 }}>{campsite.description}</Text>
+      </Card>
+    );
+  }
+  return <View />;
 }
 
-function RenderComments({comments}) {
-
-    const renderCommentItem = ({item}) => {
-        return (
-            <View style={{margin: 10}}>
-                <Text style={{fontSize: 14}}>{item.text}</Text>
-                <Text style={{fontSize: 12}}>{item.rating} Stars</Text>
-                <Text style={{fontSize: 12}}>{`-- ${item.author}, ${item.date}`}</Text>
-            </View>
-        );
-    };
-
+function RenderComments({ comments }) {
+  const renderCommentItem = ({ item }) => {
     return (
-        <Card title='Comments'>
-            <FlatList
-                data={comments}
-                renderItem={renderCommentItem}
-                keyExtractor={item => item.id.toString()}
-            />
-        </Card>
+      <View style={{ margin: 10 }}>
+        <Text style={{ fontSize: 14 }}>{item.text}</Text>
+        <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+        <Text
+          style={{ fontSize: 12 }}
+        >{`-- ${item.author}, ${item.date}`}</Text>
+      </View>
     );
+  };
+
+  return (
+    <Card title="Comments">
+      <FlatList
+        data={comments}
+        renderItem={renderCommentItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </Card>
+  );
 }
 class CampsiteInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorite: false,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES,
-            comments: COMMENTS
-        };
-    }
+  static navigationOptions = {
+    title: "Campsite Information",
+  };
 
-    static navigationOptions = {
-        title: 'Campsite Information'
-    }
-
-    render() {
-        const campsiteId = this.props.navigation.getParam('campsiteId');
-        const campsite = this.state.campsites.filter(campsite => campsite.id === campsiteId)[0];
-        const comments = this.state.comments.filter(comment => comment.campsiteId === campsiteId);
-        return (
-            <ScrollView>
-                <RenderCampsite campsite={campsite} />
-                <RenderComments comments={comments} />
-            </ScrollView>
-        );
-    }
+  render() {
+    const campsiteId = this.props.navigation.getParam("campsiteId");
+    const campsite = this.props.campsites.campsites.filter(
+      (campsite) => campsite.id === campsiteId
+    )[0];
+    const comments = this.props.comments.comments.filter(
+      (comment) => comment.campsiteId === campsiteId
+    );
+    return (
+      <ScrollView>
+        <RenderCampsite
+          campsite={campsite}
+          favorite={this.state.favorite}
+          markFavorite={() => this.markFavorite()}
+        />
+        <RenderComments comments={comments} />
+      </ScrollView>
+    );
+  }
 }
 
-export default CampsiteInfo;
+export default connect(mapStateToProps)(CampsiteInfo);
